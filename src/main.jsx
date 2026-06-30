33f6bcd993889536d73c479166642f90f2186c07
eCLIPPS-Admin <admin@teachtheway.org>
2026-06-29 23:47:16 -0400

    Set up ReactDOM rendering for ECLIPPSApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ECLIPPSApp from '../eclipps-v3-2.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ECLIPPSApp />
+  </React.StrictMode>
+)
