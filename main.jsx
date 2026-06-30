71eaa0c1c1264b678c22c00c9219bd6e9c6874cb
eCLIPPS-Admin <admin@teachtheway.org>
2026-06-29 23:17:33 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ECLIPPSApp from './ECLIPPSApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ECLIPPSApp />
+  </React.StrictMode>
+)
